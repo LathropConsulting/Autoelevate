4f4aa9e3a5b5aa58cb2e271cc281a2b741e5bbbd
BeauregardJones <BeauregardJones@users.noreply.github.com>
2020-05-08 09:09:17 -0500

    Added more structure and directory support.

--- Install-Autoelevate.ps1
+++ Install-Autoelevate.ps1
@@ -1,2 +1,9 @@
-$arguments = "/i `"C:\Path\To\AESetup.msi`" /quiet /lv AEInstallLog.log LICENSE_KEY=`"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjA0NzMzYTQwLTgxMWYtNDhiMi1iOTdmLTliZTBlM2JlNTNkNSIsIm5hbWUiOiJMYXRocm9wIEkuVC4gQ29uc3VsdGluZyIsImlhdCI6MTU4NjM3MDk5NX0.DPHnKha9bv-vQjbmjJFvC0tEz6CmvM6TmjWStHfsbIc`" COMPANY_NAME=`"Penn Thomas, PC`" COMPANY_INITIALS=`"PTPC`" LOCATION_NAME=`"Headquarters`" AGENT_MODE=`"live`""
+if (!(test-path $env:ToolsDir)) {
+    New-Item -ItemType Directory -Path $env:ToolsDir
+}
+
+Move-Item AESetup.msi $env:ToolsDir\AESetup.msi
+
+$arguments = "/i $env:ToolsDir /quiet /lv AEInstallLog.log LICENSE_KEY=`"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjA0NzMzYTQwLTgxMWYtNDhiMi1iOTdmLTliZTBlM2JlNTNkNSIsIm5hbWUiOiJMYXRocm9wIEkuVC4gQ29uc3VsdGluZyIsImlhdCI6MTU4NjM3MDk5NX0.DPHnKha9bv-vQjbmjJFvC0tEz6CmvM6TmjWStHfsbIc`" COMPANY_NAME=`"Penn Thomas, PC`" COMPANY_INITIALS=`"PTPC`" LOCATION_NAME=`"Headquarters`" AGENT_MODE=`"live`""
+
 Start-Process msiexec.exe -ArgumentList $arguments -Wait
