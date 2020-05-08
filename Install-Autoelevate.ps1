5a26d64e5d15486f7b59cfae5d395657d539d610
BeauregardJones <BeauregardJones@users.noreply.github.com>
2020-05-08 09:22:54 -0500

    Changed mode from "live" to "audit".

--- Install-Autoelevate.ps1
+++ Install-Autoelevate.ps1
@@ -6,6 +6,6 @@ if (!(test-path $env:ToolsDir)) {
 
 Move-Item AESetup.msi $env:ToolsDir\AESetup.msi
 
-$arguments = "/i $env:ToolsDir /quiet /lv AEInstallLog.log LICENSE_KEY=`"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjA0NzMzYTQwLTgxMWYtNDhiMi1iOTdmLTliZTBlM2JlNTNkNSIsIm5hbWUiOiJMYXRocm9wIEkuVC4gQ29uc3VsdGluZyIsImlhdCI6MTU4NjM3MDk5NX0.DPHnKha9bv-vQjbmjJFvC0tEz6CmvM6TmjWStHfsbIc`" COMPANY_NAME=`"Penn Thomas, PC`" COMPANY_INITIALS=`"PTPC`" LOCATION_NAME=`"Headquarters`" AGENT_MODE=`"live`""
+$arguments = "/i $env:ToolsDir /quiet /lv AEInstallLog.log LICENSE_KEY=`"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjA0NzMzYTQwLTgxMWYtNDhiMi1iOTdmLTliZTBlM2JlNTNkNSIsIm5hbWUiOiJMYXRocm9wIEkuVC4gQ29uc3VsdGluZyIsImlhdCI6MTU4NjM3MDk5NX0.DPHnKha9bv-vQjbmjJFvC0tEz6CmvM6TmjWStHfsbIc`" COMPANY_NAME=`"Penn Thomas, PC`" COMPANY_INITIALS=`"PTPC`" LOCATION_NAME=`"Headquarters`" AGENT_MODE=`"audit`""
 
 Start-Process msiexec.exe -ArgumentList $arguments -Wait
